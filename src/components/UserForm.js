4588ee12ecc841b396a4c9507cd56c9a053ea0ce
Manuel <tofuah@gmail.com>
2021-09-08 11:57:54 +0100

    added components with UserForm and UserList docs

---
+++ src/components/UserForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+ function UserForm() {
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [gender, setGender] = useState("");
+
+
+
+
+ }
+export default UserForm;
