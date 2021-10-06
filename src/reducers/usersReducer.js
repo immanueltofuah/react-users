05cf0e80819b5610e5fed00f56638c16e3b479ee
Manuel <tofuah@gmail.com>
2021-10-06 12:11:07 +0100

    added reducer function

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,18 @@
+initialState= {
+users:[
+    {name:"herbert",
+    email:"himself@gmail.com",
+    gen: "20"},
+
+    {name:"herbert",
+    email:"himself@gmail.com",
+    gen: "20"}
+]
+
+}
+
+const usersReducer =(state = initialState, action)=>{
+return state
+
+
+};
