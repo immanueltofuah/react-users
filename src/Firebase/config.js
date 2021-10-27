6f0621137541ce4b8c6a667dfe1d917a64cc963a
Manuel <tofuah@gmail.com>
2021-10-27 12:06:50 +0100

    import firebase and paste config code

---
+++ src/Firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBbxCjgskB0BiV6ougxRaYl5kgKFbAfUcU",
+    authDomain: "react-users-d5539.firebaseapp.com",
+    projectId: "react-users-d5539",
+    storageBucket: "react-users-d5539.appspot.com",
+    messagingSenderId: "699310707591",
+    appId: "1:699310707591:web:d534b7f006aea940e1f922"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
