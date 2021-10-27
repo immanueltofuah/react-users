06f5dedb6365164e2825747ac3d4ed018d5938ab
Manuel <tofuah@gmail.com>
2021-10-27 12:23:48 +0100

    import firebase, thunk, firestore

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,8 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import firebase from "../Firebase/config";
+import { getFirebase, reduxReactFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import thunk from "redux-thunk";
 import usersReducer from "../reducers/usersReducer";
 
 
