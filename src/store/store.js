cc7174658ff90aefa247278d88e711e7fb421c1c
Manuel <tofuah@gmail.com>
2021-10-27 12:41:54 +0100

    changed Firebase to firebase

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware, compose} from "redux";
-import firebase from "../Firebase/config";
+import firebase from "../firebase/Config";
 import { getFirebase, reduxReactFirebase } from "react-redux-firebase";
 import { getFirestore, reduxFirestore } from "redux-firestore";
 import thunk from "redux-thunk";
