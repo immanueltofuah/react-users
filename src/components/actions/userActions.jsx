72e3321b2736a54954b6dd73ab32a886e483114e
Manuel <tofuah@gmail.com>
2021-11-10 11:26:10 +0000

    fix error

--- src/components/actions/userActions.jsx
+++ src/components/actions/userActions.jsx
@@ -1,58 +1,56 @@
 //export const ADD_USER = "ADD_USER";
 
 export const addUser = (user) => {
-	return (dispatch, state, { getFirestore }) => {
-		getFirestore()
-			.collection("users")
-			.add(user)
-			.then(() => {
-				// dispatch({
-				// 	type: "ADD_USER",
-				// 	payload: user,
-				// });
-			});
-	};
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .add(user)
+      .then(() => {});
+  };
 
-	// console.log("addUser", user);
-	// //user.id = Math.random().toString();
+  // console.log("addUser", user);
+  // //user.id = Math.random().toString();
 };
 
 export const deleteUser = (userId) => {
-	return (dispatch, state, { getFirestore }) => {
-		getFirestore()
-			.collection("users")
-			.doc(userId)
-			.delete()
-			.then(() => {});
-	};		
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .doc(userId)
+      .delete()
+      .then(() => {});
+  };
 
-// 	return {
-// 		type: "DELETE_USER",
-// 		payload: userId,
-// 	};
+  // 	return {
+  // 		type: "DELETE_USER",
+  // 		payload: userId,
+  // 	};
 };
 
 export const editUser = (userId, updatedUser) => {
-	return {
-		type: "EDIT_USER",
-		payload: { userId, updatedUser },
-	};
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore();
+  };
+  // return {
+  // 	type: "EDIT_USER",
+  // 	payload: { userId, updatedUser },
+  // };
 };
 
 export const getAllUsers = () => {
-	return (dispatch, state, { getFirestore }) => {
-		getFirestore()
-			.collection("users")
-			.onSnapshot(
-				(snapshot) => {
-					let users = [];
-					snapshot.forEach((doc) => {
-						users.push(doc.data());
-					});
-					console.log(users);
-					dispatch({ type: "SET_ALL_USERS", payload: users });
-				},
-				(error) => {}
-			);
-	};
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .onSnapshot(
+        (snapshot) => {
+          let users = [];
+          snapshot.forEach((doc) => {
+            users.push(doc.data());
+          });
+          console.log(users);
+          dispatch({ type: "SET_ALL_USERS", payload: users });
+        },
+        (error) => {}
+      );
+  };
 };
